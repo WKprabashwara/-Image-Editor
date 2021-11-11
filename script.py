0c239928d68574c77498e3b3ecb5fc4c7a6dabf9
Rivibibu Prabashwara <92269011+WKprabashwara@users.noreply.github.com>
2021-11-11 08:37:59 +0530

    Update script.py

--- script.py
+++ script.py
@@ -15,10 +15,14 @@ For more click help....</b>"""
 
 
 © @TroJanzHEX"""
-    ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
+    ABOUT_MSG = """
+    
+-<b>My Name : Image Editor</b>
 
-⭕️<b>Language :</b> <code>Python3</code>
+-<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
+-<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
+-<b>Source Code :</b> 👉 <a href='https://github.com/WKprabashwara/-Image-Editor'>Click Here</a>
+
+"""
